0b946a23d09c29f9924c5db0d0f467c07b59d5ea
Tim Showers <showerst@gmail.com>
2019-12-23 10:13:18 -0500

    PR: Adjust init for new website

--- openstates/pr/__init__.py
+++ openstates/pr/__init__.py
@@ -19,24 +19,19 @@ class PuertoRico(Jurisdiction):
     }
     legislative_sessions = [
         {
-            "_scraped_name": "2009-2012",
-            "identifier": "2009-2012",
-            "name": "2009-2012 Session",
-        },
-        {
-            "_scraped_name": "2013-2016",
+            "_scraped_name": "2013 - 2016",
             "identifier": "2013-2016",
             "name": "2013-2016 Session",
         },
         {
-            "_scraped_name": "2017-2020",
+            "_scraped_name": "2017 - 2020",
             "identifier": "2017-2020",
             "name": "2017-2020 Session",
             "start_date": "2017-01-02",
             "end_date": "2021-01-01",
         },
     ]
-    ignored_scraped_sessions = ["2005-2008", "2001-2004", "1997-2000", "1993-1996"]
+    ignored_scraped_sessions = ["Seleccione Cuatrenio"]
 
     def get_organizations(self):
         legislature_name = "Legislative Assembly of Puerto Rico"
@@ -51,6 +46,6 @@ class PuertoRico(Jurisdiction):
 
         # this URL should work even for future sessions
         return url_xpath(
-            "http://www.oslpr.org/legislatura/tl2013/buscar_2013.asp",
-            '//select[@name="URL"]/option/text()',
+            "http://www.tucamarapr.org/dnncamara/web/ActividadLegislativa/TramiteLegislativo.aspx",
+            '//select[@id="dnn_ctr1157_ViewWebMeasures_WebMeasures1_ddlTerm"]/option/text()',
         )
